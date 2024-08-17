bf4975a242c861d4285ad1abbe02b6dafa8838f8
Jan Woll <144726069+Trawollta@users.noreply.github.com>
2024-08-17 18:17:31 +0200

    different quality implemented

--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { VideoService } from '../services/video-service';
@@ -8,13 +8,13 @@ import { Video } from '../interfaces/video';
 @Component({
   selector: 'app-video-detail',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './video-detail.component.html',
   styleUrls: ['./video-detail.component.scss']
 })
 export class VideoDetailComponent implements OnInit {
   video?: Video;
-  selectedQuality: string = '480p'; 
+  selectedQuality: string = '1080p';
 
   constructor(
     private route: ActivatedRoute,
@@ -27,12 +27,36 @@ export class VideoDetailComponent implements OnInit {
     if (videoId) {
       this.videoService.getVideo(+videoId).subscribe((data: Video) => {
         this.video = data;
+        this.autoSelectQuality();
       });
     }
+    console.log('Video URL für die ausgewählte Qualität:', this.getVideoUrl());
   }
 
   getVideoUrl(): string {
-    return this.video?.video_files[this.selectedQuality] || '';
+    const url = this.video?.video_files[this.selectedQuality] || '';
+    return url;
+  }
+
+  autoSelectQuality(): void {
+    if (!this.getVideoUrl()) {
+      if (this.video?.video_files['1080p']) {
+        this.selectedQuality = '1080p';
+      } else if (this.video?.video_files['720p']) {
+        this.selectedQuality = '720p';
+      } else if (this.video?.video_files['480p']) {
+        this.selectedQuality = '480p';
+      }
+    }
+  }
+
+  onQualityChange(): void {
+    const videoElement = document.querySelector('video');
+    if (videoElement) {
+      videoElement.src = this.getVideoUrl();
+      videoElement.load(); 
+      videoElement.play();
+    }
   }
 
   goBack(): void {
